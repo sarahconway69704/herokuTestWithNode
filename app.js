2ada01dd14b374fe8ae4129e62d5b3086e58ba65
Sarah Conway <sarahconway@Sarahs-MBP.fc.ca>
2019-09-23 19:32:05 -0400

    pt.1 done

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
-const http = require('http');
+const express = require('express');
+const path = require('path'); // path lets us navigate the file system / folders
 
-const hostname = '127.0.0.1';
-const port = 3030;
+// heroku assigns a port when it deploys via the process (environment variables, coming soon )
+// locally this will run @ port 3000; remotely it'll run wherever heroku tells it to run
+const port = process.env.PORT || 3000; //double pipe means "or"
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
+const app = express();
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.use(express.static('public'));
+
+// a forward slash is the home route (same as index.html)
+app.get('/', (req, res) => {
+  console.log('at the home route');
+  res.sendfile(path.join(__dirname + '/views/index.html'));
+  // this builds localhost:3000/views/index.html
+})
+
+
+app.get('/contact', (req, res) => {
+  res.sendfile(path.join(__dirname + '/views/contact.html'));
+  console.log('at the contact route');
+})
+
+app.get('/portfolio', (req, res) => {
+  res.send('on the portfolio page');
+  console.log('at the portfolio route');
+})
+
+app.listen(port, () => {
+  console.log(`Server running at ${port}`)
+})
